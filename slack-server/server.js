0f8d6114b6a62d9b9849fccdd9a09e37f03f328d
LazyIce <1542752218@qq.com>
2019-06-11 00:30:21 -0700

    add database

--- slack-server/server.js
+++ slack-server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import SERVER from './schema';
+import models from './models';
 
 const APP = express();
 
@@ -10,9 +11,11 @@ SERVER.applyMiddleware({
 
 const PORT = 8181 || process.env;;
 
-APP.listen(PORT, () => {
-    console.log(`The server has started on port: ${PORT}`);
-    console.log(`http://localhost:${PORT}/graphql`);
+models.sequelize.sync({ force: true }).then(() => {
+    APP.listen(PORT, () => {
+        console.log(`The server has started on port: ${PORT}`);
+        console.log(`http://localhost:${PORT}/graphql`);
+    });  
 });
 
 export default APP;
